dc7c726b14608c0d01e95e5150b0e00f9f759dc9
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2026-04-14 18:43:25 +0300

    init

---
+++ backend/src/main/java/com/quizapp/backend/WebAppQuizApplication.java
@@ -0,0 +1,12 @@
+package com.quizapp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppQuizApplication.class, args);
+    }
+}
