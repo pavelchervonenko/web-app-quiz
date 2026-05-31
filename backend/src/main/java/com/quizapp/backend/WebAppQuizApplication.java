0f455d235731449b2d0247ba050937716d28251f
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2026-06-01 00:50:29 +0300

    meow :)

--- backend/src/main/java/com/quizapp/backend/WebAppQuizApplication.java
+++ backend/src/main/java/com/quizapp/backend/WebAppQuizApplication.java
@@ -3,7 +3,10 @@ package com.quizapp.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 @SpringBootApplication
+@EnableJpaAuditing
 public class WebAppQuizApplication {
 
     public static void main(String[] args) {
