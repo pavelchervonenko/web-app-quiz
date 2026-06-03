3f40e8de96c6c748736e61e9272c21710291833c
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2026-06-03 15:58:36 +0300

    add controller for auth

---
+++ backend/src/main/java/com/quizapp/backend/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.quizapp.backend.controller;
+
+import com.quizapp.backend.dto.auth.AuthLoginRequest;
+import com.quizapp.backend.dto.auth.AuthRegisterRequest;
+import com.quizapp.backend.dto.auth.AuthResponse;
+
+import com.quizapp.backend.service.AuthService;
+
+import jakarta.validation.Valid;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.CREATED)
+    public AuthResponse auth(@RequestBody @Valid AuthLoginRequest dto) {
+        return authService.login(dto);
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public AuthResponse register(@RequestBody @Valid AuthRegisterRequest dto) {
+        return authService.register(dto);
+    }
+}
