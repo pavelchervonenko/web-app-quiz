99fd90b2f29686e84793d8deb2104d8310fcc662
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2026-06-03 15:56:56 +0300

    add main security config: filter chain and auth manager

--- backend/src/main/java/com/quizapp/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/quizapp/backend/config/SecurityConfig.java
@@ -1,28 +1,88 @@
 package com.quizapp.backend.config;
 
+import com.quizapp.backend.handler.SecurityAccessDeniedHandler;
+import com.quizapp.backend.handler.SecurityAuthenticationEntryPoint;
+import com.quizapp.backend.service.CustomUserDetailService;
+
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomUserDetailService userDetailService;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final SecurityAuthenticationEntryPoint authenticationEntryPoint;
+
+    private final SecurityAccessDeniedHandler accessDeniedHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
             .csrf(csrf -> csrf.disable())
+            .sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
             .authorizeHttpRequests(auth ->
                 auth
                     .requestMatchers(
                         "/swagger-ui.html",
                         "/swagger-ui/**",
-                        "/api-docs/**").permitAll()
+                        "/api/auth/**",
+                        "/api-docs/**"
+                    ).permitAll()
                     .anyRequest().authenticated())
+            .exceptionHandling(exceptions -> exceptions
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .accessDeniedHandler(accessDeniedHandler)
+            )
+            .oauth2ResourceServer(oauth2 ->
+                oauth2.jwt(jwt ->
+                    jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                    .authenticationEntryPoint(authenticationEntryPoint)
+                    .accessDeniedHandler(accessDeniedHandler)
+            )
             .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        var builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        builder.userDetailsService(userDetailService).passwordEncoder(passwordEncoder);
+
+        return builder.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+        authoritiesConverter.setAuthoritiesClaimName("role");
+        authoritiesConverter.setAuthorityPrefix("ROLE_");
+
+        JwtAuthenticationConverter authenticationConverter = new JwtAuthenticationConverter();
+
+        authenticationConverter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
+
+        return authenticationConverter;
     }
 }
