334731ba4d72b8d1c3044d8026ee99e3d4548d24
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2026-06-03 16:00:37 +0300

    add basic mapper for user entity

---
+++ backend/src/main/java/com/quizapp/backend/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.quizapp.backend.mapper;
+
+import com.quizapp.backend.dto.user.CurrentUserDTO;
+import com.quizapp.backend.model.User;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface UserMapper {
+
+    CurrentUserDTO toCurrentUserDTO(User user);
+}
