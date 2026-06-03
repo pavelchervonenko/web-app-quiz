01d41adb66d48e35d40fb7bbc426d999d046ccf2
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2026-06-03 15:50:07 +0300

    update dependencies

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -47,12 +47,13 @@ dependencies {
 
     annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
 
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+
     implementation("org.springframework.boot:spring-boot-flyway")
     implementation("org.flywaydb:flyway-core")
     runtimeOnly("org.flywaydb:flyway-database-postgresql")
 
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
-    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
 }
 
 tasks.withType<Test> {
